8cc9ece5b95736071721ac70721e3d7625ee2036
Karim Luy <karim.luy@he-arc.ch>
2017-03-21 16:41:48 +0100

    base project

---
+++ GhostLight/Assets/Scripts/LevelControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControls : MonoBehaviour {
+    private Rigidbody level;
+	// Use this for initialization
+	void Start () {
+        level = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Update is called regardless of frame activity
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            float x = Input.GetAxis("Vertical");
+            float z = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Rotate");
+            transform.Rotate(new Vector3(x, y, -z));
+        }
+        
+    }
+}
