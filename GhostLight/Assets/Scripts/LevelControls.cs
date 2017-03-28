ffdf09e2fe4cc176b7e1cb222264d5c88db90be6
Karim Luy <karim.luy@he-arc.ch>
2017-03-28 18:34:41 +0200

    have snapping almost working

--- GhostLight/Assets/Scripts/LevelControls.cs
+++ GhostLight/Assets/Scripts/LevelControls.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class LevelControls : MonoBehaviour {
-    public Toggle snap;
+    private bool snap;
+    private Vector3[] snapAngles;
 	// Use this for initialization
 	void Awake () {
-
+        snap = false;
+        snapAngles = new Vector3[] { new Vector3(0,0,0), new Vector3(90, 0, 0), new Vector3(0, 90, 90), new Vector3(0, 270, 270) };
 	}
 	
 	// Update is called once per frame
@@ -18,13 +20,53 @@ public class LevelControls : MonoBehaviour {
     // Update is called regardless of frame activity
     void FixedUpdate()
     {
-        if (snap.isOn)
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Rotate");
+        float angleX = transform.rotation.eulerAngles.x;
+        float angleY = transform.rotation.eulerAngles.y;
+        float angleZ = transform.rotation.eulerAngles.z;
+        if (Input.GetKeyDown(KeyCode.Space)) { snap = !snap; }
+        if (!snap)
         {
-            float x = Input.GetAxis("Vertical");
-            float z = Input.GetAxis("Horizontal");
-            float y = Input.GetAxis("Rotate");
-            transform.Rotate(new Vector3(x, y, -z));
+            print("X: " + angleX + " Y: " + angleY + " Z: " + angleZ);
+            RotateLevel(x,y,z);
+        }
+        else {
+            RotateLevel(x,y,z);
+            //--------------------SNAPS LEVEL POSITION TO NEAREST FACE-------------------------------------------------
+            {
+                print("Distance to origin: " + Vector3.Distance(transform.eulerAngles, snapAngles[3]));
+                if (Vector3.Distance(transform.eulerAngles, snapAngles[0]) < 5.0f || Vector3.Distance(transform.eulerAngles, snapAngles[0]) > 355.0f)
+                {
+                    transform.eulerAngles = snapAngles[0];
+                    snap = false;
+                }
+                if (Vector3.Distance(transform.eulerAngles, snapAngles[1]) < 10.0f || Vector3.Distance(transform.eulerAngles, snapAngles[1]) > 254.0f)
+                {
+                    transform.eulerAngles = snapAngles[1];
+                    snap = false;
+                }
+                if (Vector3.Distance(transform.eulerAngles, snapAngles[2]) < 5.0f)
+                {
+                    transform.eulerAngles = snapAngles[2];
+                    snap = false;
+                }
+                if (Vector3.Distance(transform.eulerAngles, snapAngles[3]) < 5.0f)
+                {
+                    transform.eulerAngles = snapAngles[3];
+                    snap = false;
+                }
+
+            }
+           
         }
         
+    }
+    void RotateLevel(float x, float y, float z) {
+        transform.Rotate(new Vector3(x, y, -z));
+    }
+    public void ChangeSnap() {
+        if (Input.GetKey(KeyCode.Space)) { print("reversing snap"); snap = !snap; }
     }
 }
