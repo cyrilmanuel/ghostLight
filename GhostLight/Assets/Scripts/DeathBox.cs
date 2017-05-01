906689dbdc23769d2696ed41fc1cfeff68279f8e
Karim Luy <karim.luy@he-arc.ch>
2017-05-01 16:23:01 +0200

    more scripting handled

---
+++ GhostLight/Assets/Scripts/DeathBox.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathBox : MonoBehaviour {
+    private GameObject player;
+    private GameObject levelbase;
+    private BoxCollider box;
+    public float height;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Awake() {
+        player = GameObject.Find("Player");
+        levelbase = GameObject.Find("LevelBottom");
+        box = GetComponent<BoxCollider>() as BoxCollider;
+        box.size = new Vector3(levelbase.transform.localScale.x, height, levelbase.transform.localScale.z);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            player.GetComponent<PlayerController>().alive = false;
+        }
+    }
+}
